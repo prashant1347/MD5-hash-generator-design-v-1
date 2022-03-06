2fab081c722625b9ac1d3ec9224a13e8107d0c65
prashant1347 <84553258+prashant1347@users.noreply.github.com>
2022-03-06 17:57:21 +0545

    Add files via upload

---
+++ md5 hash generator/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  
+  <div class="container">
+    <div class="row">
+        <h2>MD5 Hash Generator</h2>
+    </div>
+    <div class="row-2">
+    <div class="form-group">
+  <label for="exampleFormControlTextarea3">Use this generator to create an MD5 hash of a string:
+</label>
+  <textarea class="form-control" id="exampleFormControlTextarea3" rows="10"></textarea>
+</div>
+<button type="button" class="btn btn-primary">Generate&#8594;</button>
+    </div>
+  </div>
+   
+ <style>
+     
+     
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto:wght@100;400&display=swap');*{
+    font-family: Montserrat;
+    font-weight: lighter;
+}
+      .row{
+         display: flex;
+          margin-top: 72px;
+         justify-content: center;
+         align-items: center;
+     }
+    label{
+        font-weight: lighter;
+    }
+   
+    </style>
+
+</html>
